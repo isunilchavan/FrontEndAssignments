9293c3046a62ca5e0acff5dc528d285257c08917
Sunil Chavan <139366162+isunilchavan@users.noreply.github.com>
2023-09-29 16:57:14 +0530

    location where I spent my money

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,13 +1,25 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div className="App">
-      <h2>Expense Items</h2>
-      <ul>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-      </ul>
-    </div>
-  );
+    <div className="ExpenseItem">
+    <h2 className="header">Expense Items</h2>
+    <ul className="expense-list">
+      <li>
+        Food Rs 10
+        <span className="location"><br></br>< strong>Location:(Grocery Store)</strong></span>
+        </li>
+      
+      <li>
+        Petrol Rs 100
+        <span className="location"><br></br>< strong>Location:(Gas Station)</strong></span>
+        </li>
+      <li>
+        Movies Rs 200
+        <span className="location"><br></br>< strong>Location:(Movie Theater)</strong></span>
+        </li>
+    </ul>
+  </div>
+);
 }
 export default ExpenseItem;
