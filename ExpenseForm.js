16658e07e49436975a50a7b30c3692f3828805d3
Sunil Chavan <139366162+isunilchavan@users.noreply.github.com>
2023-10-04 18:25:54 +0530

    adding states to all the components

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,28 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+
   const nameChangeHandler = (event) => {
-    console.log(event.target.value);
+    setName(event.target.value);
   };
-  
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const locationChangeHandler = (event) => {
+    setLocation(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+
   return (
     <form className="form-container">
       <div className="form-field">
         <label>Name</label>
-        <input type="text" className="form-input" onChange={nameChangeHandler} />
+        <input
+          type="text"
+          className="form-input"
+          onChange={nameChangeHandler}
+          value={name}
+        />
       </div>
       <div className="form-field">
         <label>Amount</label>
-        <input type="number" className="form-input" />
+        <input
+          type="number"
+          className="form-input"
+          onChange={amountChangeHandler}
+          value={amount}
+        />
       </div>
       <div className="form-field">
         <label>Location</label>
-        <input type="text" className="form-input" />
+        <input
+          type="text"
+          className="form-input"
+          onChange={locationChangeHandler}
+          value={location}
+        />
       </div>
       <div className="form-field">
         <label>Date</label>
-        <input type="date" className="form-input" />
+        <input
+          type="date"
+          className="form-input"
+          onChange={dateChangeHandler}
+          value={date}
+        />
       </div>
       <button type="submit" className="form-button">
         Add Expense
