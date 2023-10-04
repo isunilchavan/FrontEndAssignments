5b8d6b4c52a2d7b358bc906588a2456f724e415d
Sunil Chavan <139366162+isunilchavan@users.noreply.github.com>
2023-10-04 14:14:57 +0530

    changing amount

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,23 +1,30 @@
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate.js';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate.js";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [amount, setAmount] = useState(props.amount);
 
   const deleteExpense = () => {
-    console.log('ExpenseDeleted');
+    console.log("ExpenseDeleted");
+  };
+  
+  const changeAmount = () => {
+    setAmount(100);
+    console.log("Amount changed");
   };
   return (
     <div className="ExpenseItem">
       <ExpenseDate date={props.date} />
       <div>
         <h1>{props.name}</h1>
-        <p>Amount: Rs {props.amount}</p>
+        <p>Amount:Rs {amount}</p>
         <p>Location: {props.location}</p>
         <button onClick={deleteExpense}>Delete Expense</button>
+        <button onClick={changeAmount}>Change Amount</button>
       </div>
     </div>
   );
 }
-
 export default ExpenseItem;
